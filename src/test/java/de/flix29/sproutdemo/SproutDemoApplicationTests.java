c841356f8c429038c0341d1342ac84dc389a1c17
flix29 <flix.1895@web.de>
2025-08-23 21:06:29 +0200

    add test to verify existence of SproutProductEntityRepository bean

--- src/test/java/de/flix29/sproutdemo/SproutDemoApplicationTests.java
+++ src/test/java/de/flix29/sproutdemo/SproutDemoApplicationTests.java
@@ -1,13 +1,18 @@
 package de.flix29.sproutdemo;
 
+import de.flix29.sproutdemo.entities.generated.repositories.SproutProductEntityRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class SproutDemoApplicationTests {
+    @Autowired
+    SproutProductEntityRepository repo;
 
-    @Test
-    void contextLoads() {
+    @Test void repoBeanExists() {
+        assertThat(repo).isNotNull();
     }
-
 }
