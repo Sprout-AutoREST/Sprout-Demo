2730f0f7edd050d7f1bdb7b0ba41fdeeae89c346
flix29 <flix.1895@web.de>
2025-08-24 22:02:00 +0200

    add tests to verify existence of SproutProductEntityService and SproutProductEntityController beans

--- src/test/java/de/flix29/sproutdemo/SproutDemoApplicationTests.java
+++ src/test/java/de/flix29/sproutdemo/SproutDemoApplicationTests.java
@@ -1,6 +1,8 @@
 package de.flix29.sproutdemo;
 
+import de.flix29.sproutdemo.entities.generated.controller.SproutProductEntityController;
 import de.flix29.sproutdemo.entities.generated.repositories.SproutProductEntityRepository;
+import de.flix29.sproutdemo.entities.generated.services.SproutProductEntityService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,10 +11,26 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class SproutDemoApplicationTests {
-    @Autowired
-    SproutProductEntityRepository repo;
 
-    @Test void repoBeanExists() {
+    @Autowired
+    private SproutProductEntityRepository repo;
+    @Autowired
+    private SproutProductEntityService service;
+    @Autowired
+    private SproutProductEntityController controller;
+
+    @Test
+    void repoBeanExists() {
         assertThat(repo).isNotNull();
+    }
+
+    @Test
+    void serviceBeanExists() {
+        assertThat(service).isNotNull();
+    }
+
+    @Test
+    void controllerBeanExists() {
+        assertThat(controller).isNotNull();
     }
 }
